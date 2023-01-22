a5867bff9b92aa191290c5a78491ee74c8f139c8
inci97 <49168504+inci97@users.noreply.github.com>
2023-01-22 22:23:35 +0300

    Update cpufreq-scx35.c

--- arch/arm/mach-sc/cpufreq-scx35.c
+++ arch/arm/mach-sc/cpufreq-scx35.c
@@ -50,7 +50,7 @@
 
 #define FREQ_TABLE_SIZE 	10
 #define DVFS_BOOT_TIME	(30 * HZ)
-#define SHARK_TDPLL_FREQUENCY	(768000)
+#define SHARK_TDPLL_FREQUENCY	(256000)
 #define TRANSITION_LATENCY	(100 * 1000) /* ns */
 
 static DEFINE_MUTEX(freq_lock);
@@ -232,15 +232,23 @@ static struct cpufreq_table_data sc8830t_cpufreq_table_data_es = {
 #else
 static struct cpufreq_table_data sc8830t_cpufreq_table_data_es_1300 = {
 	.freq_tbl = {
-		{0, 1300000},
-		{1, 1200000},
-		{2, 1000000},
-		{3, SHARK_TDPLL_FREQUENCY},
-		{4, CPUFREQ_TABLE_END},
+		{0, 1600000},
+		{1, 1500000},
+		{2, 1300000},
+		{3, 1100000},
+		{4, 1000000},
+		{5, 800000},
+		{6, 580000},
+		{7, 386000},
+		{8, CPUFREQ_TABLE_END},
 	},
 	.vddarm_mv = {
+		1300000,
+		1250000,
+		1200000,
 		1050000,
 		1000000,
+		900000,
 		900000,
 		900000,
 		900000,
@@ -434,8 +442,8 @@ static int sprd_cpufreq_verify_speed(struct cpufreq_policy *policy)
 	return cpufreq_frequency_table_verify(policy, sprd_cpufreq_conf->freq_tbl);
 }
 
-unsigned int cpufreq_min_limit = ULONG_MAX;
-unsigned int cpufreq_max_limit = 0;
+unsigned int cpufreq_min_limit = 386000;
+unsigned int cpufreq_max_limit = 1600000;
 unsigned int dvfs_score_select = 5;
 unsigned int dvfs_unplug_select = 2;
 unsigned int dvfs_plug_select = 0;
